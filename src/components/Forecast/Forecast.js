f0cb17f040d5146f08342c8ad800107f27409354
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-07 16:07:43 -0800

    add Forecast component that will be used to fetch and return weather data. Create skeleton of fetch/return function.

---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forecast = () => {
+    function getForecast() {
+        // weather data fetch will go here
+    }
+    return (
+        // JSX will go here
+    )
+}
+
+export default Forecast;
