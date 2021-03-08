63f7ed4ef0175daf499d4eb0c7094a859b229d75
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-07 16:12:41 -0800

    Build fetch request into getForecast(). Build JSX return object from fetch request.

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const Forecast = () => {
     function getForecast() {
-        // weather data fetch will go here
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+	        "method": "GET",
+	        "headers": {
+		        "x-rapidapi-key": "007ca66fabmsh50f78cebc444c66p18f481jsn99e4d7fc2fae",
+		        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	    }
+})
     }
     return (
-        // JSX will go here
+        <div>
+            <h2>Find Current Weather Conditions</h2>
+            <div>
+                {JSON.stringify(responseObj)}
+            </div>
+            <button onClick={getForecast}>Get Forecast</button>
+        </div>
     )
 }
 
