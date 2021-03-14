b9c0e6a0dd4897012d1664ea89869324f36cfc9a
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-14 16:03:33 -0700

    Fix fetch request location. Previous link contained broken queries, query now only looks for location hardcoded to Seattle

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,17 +3,18 @@ import React, { useState } from 'react';
 const Forecast = () => {
     let [responseObj, setResponseObj] = useState({});
     function getForecast() {
-        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle", {
 	        "method": "GET",
 	        "headers": {
 		        "x-rapidapi-key": "007ca66fabmsh50f78cebc444c66p18f481jsn99e4d7fc2fae",
 		        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
 	    }
-        .then(resp => resp.json())
-        .then(resp => {
-            setResponseObj(resp)
-        })
-})
+
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+        setResponseObj(resp)
+    })
     }
     return (
         <div>
