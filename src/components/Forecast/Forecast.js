f3c5ae4afd43b443f41826d4800db783891b473c
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-21 10:54:09 -0700

    move api key to .env. Add .env to gitignore. Call API key in Forecast.js using process.env

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Conditions from '../Conditions/Conditions.js'
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 const Forecast = () => {
     let [responseObj, setResponseObj] = useState({});
@@ -7,7 +8,7 @@ const Forecast = () => {
         fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle", {
 	        "method": "GET",
 	        "headers": {
-		        "x-rapidapi-key": "007ca66fabmsh50f78cebc444c66p18f481jsn99e4d7fc2fae",
+		        "x-rapidapi-key": API_KEY,
 		        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
 	    }
 
