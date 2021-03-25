248e53837d245aeb8add228271b9630fe2003b54
paul_vonhagen <pvonhagen26@gmail.com>
2021-03-24 17:38:32 -0700

    adjust logo color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Forecast from "./components/Forecast/Forecast.js"
+import Forecast from "./components/Forecast/Forecast.js";
+import Logo from "./components/Logo/Logo.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Logo />
         <h1>React Weather App</h1>
       </header>
       <main>
